6b5d6a173d4123037b966a2ceff59987956a7541
Cristiano Werner Araujo <cristianowerneraraujo@gmail.com>
2015-11-18 09:39:39 -0200

    Adding verification.
    Including setup.

--- csrgen.py
+++ csrgen.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
+import sys
 import argparse
 import pexpect
 from commands import getoutput as cmd
 
-parser = argparse.ArgumentParser(description="A wrapper to the openssl csr generation")
+parser = argparse.ArgumentParser(description="A wrapper to the openssl CSR() generation")
 parser.add_argument("-name", help="key name", dest="name", default="certificate")
 parser.add_argument("-country", help="Country Name", dest="country", default="BR")
 parser.add_argument("-state", help="State or Province Name ", dest="state", default="Rio Grande do Sul")
@@ -14,31 +15,24 @@ parser.add_argument("-unit", help="Organizational unit name", dest="unit", defau
 parser.add_argument("-cname", help="Common Name", dest="cname", default="Cristiano W. Araujo")
 parser.add_argument("-email", help="Email Address", dest="email", default="cristianowerneraraujo@gmail.com")
 parser.add_argument("-challenge", help="A challenge password (extra)", dest="challenge", default="")
-parser.add_argument("-opt_company", help="An optional company name", dest="opt_company", default="")
+parser.add_argument("-opt_company", help="An optional company name (extra)", dest="opt_company", default="")
+parser.add_argument("-verify", help="print out CSR information", dest="verify", action="store_true")
+
 args = parser.parse_args()
+for arg in dir(args):
+    if arg[0] != "_":
+        locals()[arg] = args.__getattribute__(arg)
 
-print cmd("openssl genrsa -out " + args.name + ".key 2048")
+def generate_csr():
+    cmd("openssl genrsa -out " + args.name + ".key 2048")
+    child = pexpect.spawn("openssl req -new -sha256 -key " + args.name + ".key -out " + args.name + ".csr")
+    for arg in [country, state, city, org, unit, cname, email, challenge, opt_company]:
+        child.sendline(arg)
+        child.expect("\]:")
 
-child = pexpect.spawn("openssl req -new -sha256 -key " + args.name + ".key -out " + args.name + ".csr")
-print "1"
-#print child.expect("[AU]:")
-child.sendline(args.country)
-print child.expect("\]:")
-child.sendline(args.state)
-print child.expect("\]:")
-child.sendline(args.city)
-print child.expect("\]:")
-child.sendline(args.org)
-print child.expect("\]:")
-child.sendline(args.unit)
-print child.expect("\]:")
-child.sendline(args.cname)
-print child.expect("\]:")
-child.sendline(args.email)
-print child.expect("\]:")
-child.sendline(args.cname)
-print child.expect("\]:")
-child.sendline(args.challenge)
-print child.expect("\]:")
-child.sendline(args.opt_company)
-print child.read()
+def verify_csr():
+    print cmd("openssl req -in " + name + ".csr -noout -text")
+
+generate_csr()
+if verify:
+    verify_csr()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name=csr_gen,
+    version=0.1,
+    packages=$PackageList,$PackageDirs
+    url=https://github.com/cristianowa/csr_generator,
+    license=MIT,
+    author=Cristiano Werner Araújo,
+    author_email=cristianowerneraraujo@gmail.com,
+    description='$Description'
+)
